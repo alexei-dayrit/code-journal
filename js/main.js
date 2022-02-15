e4519003fc5292c06f850eb17a315603cf0a4757
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-02-14 20:54:45 -0800

    Updated submit events on the journal entry form. #1

--- js/main.js
+++ js/main.js
@@ -3,12 +3,28 @@
 
 var $photoUrl = document.querySelector('#photo');
 var $img = document.querySelector('img');
-
+var $placeholderImg = $img.getAttribute('src');
 var $submit = document.querySelector('input[type="submit"]');
+var $form = document.querySelector('form');
+var nextEntryId = 1;
 
 $photoUrl.addEventListener('input', function (event) {
   $img.setAttribute('src', event.target.value);
 });
 
 $submit.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var title = $form.elements.title.value;
+  var photo = $form.elements.photo.value;
+  var notes = $form.elements.notes.value;
+  var formValues = {
+    nextEntryId: nextEntryId,
+    title: title,
+    photo: photo,
+    notes: notes
+  };
+  data.unshift(formValues);
+  nextEntryId++;
+  $img.setAttribute('src', $placeholderImg);
+  $form.reset();
 });
