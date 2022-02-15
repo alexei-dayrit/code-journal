c228eda677b059d012671fe03d444135de9f3587
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-02-14 17:33:30 -0800

    Added event listener for submit events on the journal entry form. #1

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@
 var $photoUrl = document.querySelector('#photo');
 var $img = document.querySelector('img');
 
+var $submit = document.querySelector('input[type="submit"]');
+
 $photoUrl.addEventListener('input', function (event) {
   $img.setAttribute('src', event.target.value);
+});
+
+$submit.addEventListener('submit', function (event) {
 });
