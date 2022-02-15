8dbfcdd03033b81fa64ae202063dc5edfb72c20d
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-02-15 11:09:12 -0800

    Updated event listener for form and had it prepend to data model.

--- js/main.js
+++ js/main.js
@@ -4,27 +4,27 @@
 var $photoUrl = document.querySelector('#photo');
 var $img = document.querySelector('img');
 var $placeholderImg = $img.getAttribute('src');
-var $submit = document.querySelector('input[type="submit"]');
 var $form = document.querySelector('form');
-var nextEntryId = 1;
 
 $photoUrl.addEventListener('input', function (event) {
   $img.setAttribute('src', event.target.value);
 });
 
-$submit.addEventListener('submit', function (event) {
+$form.addEventListener('submit', function (event) {
   event.preventDefault();
   var title = $form.elements.title.value;
   var photo = $form.elements.photo.value;
   var notes = $form.elements.notes.value;
-  var formValues = {
-    nextEntryId: nextEntryId,
+  var entryId = data.nextEntryId;
+
+  var newEntry = {
+    entryId: entryId,
     title: title,
     photo: photo,
     notes: notes
   };
-  data.unshift(formValues);
-  nextEntryId++;
+  data.entries.unshift(newEntry);
+  data.nextEntryId++;
   $img.setAttribute('src', $placeholderImg);
   $form.reset();
 });
