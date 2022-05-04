3b742067e96c8b1cdb8903885119322703c7daf9
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-05-04 04:17:08 +0000

    Changed all 'vars' into const or let. #9

--- js/data.js
+++ js/data.js
@@ -1,19 +1,19 @@
 /* exported data */
 
-var data = {
+let data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1
 };
 
-var previousData = localStorage.getItem('entries-local-storage');
+const previousData = localStorage.getItem('entries-local-storage');
 if (previousData !== null) {
   data = JSON.parse(previousData);
 }
 
 function handleBeforeUnload(event) {
-  var dataJSON = JSON.stringify(data);
+  const dataJSON = JSON.stringify(data);
   localStorage.setItem('entries-local-storage', dataJSON);
 }
 
